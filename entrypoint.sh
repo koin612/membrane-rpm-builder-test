e5e545edd71a980d47bf2e7fb480c73f6f40adc8
koin612 <nikoebener@gmail.com>
2023-09-25 16:37:27 +0200

    pls

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh -l
 
 mkdir -p $GITHUB_WORKSPACE/artifacts
-rpmbuild --target "x86_x64" -bb membrane.spec --buildroot $GITHUB_WORKSPACE/artifacts
+
+rpmbuild --target "x86_x64" -bb membrane.spec
+
+mv /github/home/rpmbuild/RPMS/x86_64/*.rpm $GITHUB_WORKSPACE/artifacts

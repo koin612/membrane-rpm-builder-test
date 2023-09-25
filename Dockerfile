ba8529f642c837b9803613bf702847922c6dbbb9
koin612 <nikoebener@gmail.com>
2023-09-25 15:27:43 +0200

    debug

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,6 @@ FROM fedora:latest
 
 COPY entrypoint.sh /entrypoint.sh
 
+RUN echo "$GITHUB_WORKSPACE"
+
 ENTRYPOINT [ "/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/sh -l
 
-echo "workspace=$GITHUB_WORKSPACE"
-echo "testfile" > $GITHUB_WORKSPACE/test.test
-
 echo "Hello $1"
 time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
+echo "time=$time and workspace=$GITHUB_WORKSPACE" >> $GITHUB_OUTPUT
 
 

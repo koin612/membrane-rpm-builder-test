9084a66a403b8ffa88c1cf98a918528f94fcbe6d
koin612 <nikoebener@gmail.com>
2023-09-25 16:48:49 +0200

    update mv cmd

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ mkdir -p $GITHUB_WORKSPACE/artifacts
 
 rpmbuild --target "x86_x64" -bb membrane.spec
 
-find / -type f -name "*.rpm"
+find /github/home/rpmbuild/RPMS/x86_x64 -type f -name "*.rpm" -exec mv {} $GITHUB_WORKSPACE/artifacts \;
 
 # mv /github/home/rpmbuild/RPMS/x86_64/*.rpm $GITHUB_WORKSPACE/artifacts

7f1e394de5f63f9a0372c3a0c470bd30a5eb227c
koin612 <nikoebener@gmail.com>
2023-09-25 15:25:00 +0200

    change order

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh -l
 
+echo "workspace=$GITHUB_WORKSPACE"
+echo "testfile" > $GITHUB_WORKSPACE/test.test
+
 echo "Hello $1"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
 
-echo "workspace=$GITHUB_WORKSPACE"
-echo "testfile" > $GITHUB_WORKSPACE/test.test
 
